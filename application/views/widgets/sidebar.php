d44ac9a39e3f669e577575e20beabc3681ba5a40
tanaytourismthesis <tanaytourismthesis@gmail.com>
2018-08-10 20:23:53 +0800

    Fixed responsiveness of sidebar and main container

--- application/views/widgets/sidebar.php
+++ application/views/widgets/sidebar.php
@@ -3,22 +3,20 @@
     $menu_html = '';
 
     foreach ($user_info['menu_items'] as $menu) {
-      $menu_html .= '<a href="'.($menu['url']).'" title="'.($menu['caption']).'">'.($menu['caption']).'</a>';
+      $menu_html .= '<a href="'.($menu['url']).'" title="'.($menu['caption']).'" class="menu-item">'
+                  .'<i class="fa '.$menu['icon'].'"></i> '
+                  .'<span class="caption">'.$menu['caption'].'</span>'
+                  .'</a>';
     }
 ?>
-    <div id="sidebar" class="sidenav hidden-xs hidden-sm">
-      <a href="javascript:void(0)" id="closeSidebar">&times;</a>
+    <div id="sidebar" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn hidden-xs hidden-sm" id="closeSidebar">&times;</a>
+      <button class="navbar-toggle hidden-xs hidden-sm" id="openSidebar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
       <?php echo $menu_html; ?>
     </div>
-
-    <div id="sidebar-mobile" class="sidenav hidden-md hidden-lg">
-      <?php echo $menu_html; ?>
-    </div>
-
     <!-- Use any element to open the sidenav -->
-    <button class="navbar-toggle hidden-xs hidden-sm" id="openSidebar">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
 <?php endif; ?>
