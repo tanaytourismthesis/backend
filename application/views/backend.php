158bcbd3828f469f0776bccdb1fc7b75c341c1bc
tanaytourismthesis <tanaytourismthesis@gmail.com>
2018-10-03 14:19:29 +0800

    Initialized Dashboard

--- application/views/backend.php
+++ application/views/backend.php
@@ -65,6 +65,7 @@
 <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap-switch.min.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/moment.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/chart.min.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/autoNumeric/autoNumeric.min.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/tinymce/tinymce.min.js'); ?>"></script>
 
